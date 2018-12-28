9f1f81aab4a46f8d519df4c86678d822036ad7a0
Tyler Scott <tyduptyler13@gmail.com>
2018-12-28 16:23:07 -0700

    Checkin

--- src/EventMapper.js
+++ src/EventMapper.js
@@ -5,9 +5,9 @@
 //Get required stuff
 const mysql = require("mysql");
 const config = require("./config.js");
-const fs = require("file-system");
+const fs = require("fs");
 const asyncLib = require("async");
-
+const {Event} = require("generated/app");
 
 //Get a new connection
 const sqlPool = mysql.createPool({
@@ -19,20 +19,52 @@ const sqlPool = mysql.createPool({
 });
 
 
-module.exports.mysqlRunSetup = async function () {
+module.exports.mysqlRunSetup = async function ()
+{
 
 	let systemTemplate = await fs.readFile("file");
 
 	//Replace keys in template with valid data
 	systemTemplate = systemTemplate.replace("<<DATABASE-NAME>>", config.sqlDatabase);
 
+	let version = (function ()
+	{
+		runSqlQuery("SELECT dbinstallversion FROM ")
+	})();
+
+
+};
+
+function runSqlQuery(query)
+{
 	//sql waterfall
 	asyncLib.waterfall([
-		function (callback) {
-
+		function (callback)
+		{
+			sqlPool.getConnection(function (err, conn)
+			{
+				callback(null, conn);
+			});
+		},
+		function (conn, callback)
+		{
+			conn.query(query, function (err, results)
+			{
+				conn.release();
+				callback(err, results);
+			});
 		}
-	]);
-};
+	], function (error, results)
+	{
+		if (error)
+		{
+			console.log(error);
+		}
+
+		return results;
+
+	});
+}
 
 
 
